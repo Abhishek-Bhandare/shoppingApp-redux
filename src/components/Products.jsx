357673b2ea9df8ae97e4d18ae94c1979205bf8b9
Abhishek-Bhandare <abhishekbhandare1@gmail.com>
2024-08-19 02:44:40 +0530

    first commit

---
+++ src/components/Products.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import {useDispatch} from "react-redux"
+import { add } from '../store/cartSlice'
+
+const Products = () => {
+  const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    const fetchProducts = async () => {
+      const res = await fetch("https://fakestoreapi.com/products")
+      const data = await res.json()
+      // console.log(data)
+      setProducts(data)
+    }
+    fetchProducts()
+  },[])
+
+  const handleAdd = (product) => {
+    dispatch(add(product))
+  }
+
+  return (
+    <div className='productsWrapper'>
+    {
+      products.map((product)=>{
+        return <div className='card' key={product.id}>
+          <img src={product.image}/>
+          <h4>{product.title}</h4>
+          <h5>{product.price}</h5>
+          <button onClick={()=>handleAdd(product)} className='btn'>Add to Cart</button>
+        </div>
+      })
+    }
+    </div>
+  )
+}
+
+export default Products
